c9cacf4edc97d2fb92acbc62a063c662c637db78
shiva puthra <shiv@gmail.com>
2019-07-08 04:54:03 +0530

    downloaded Stest project from Github and modified login.java file, deleted green.png file

--- login.java
+++ login.java
@@ -2,5 +2,6 @@ public class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     System.out.println ("this is git test");
+    System.out.println("finally downloaded Stest project from Github and modified login.java file")
 	}  
 }  
